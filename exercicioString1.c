382fd8a7e4955f347757a328b5a1080d49bb9893
Rafael Freitas <rafaelsfreitas283@gmail.com>
2023-02-22 02:18:18 -0300

    new file:   Exercicio22.exe

--- exercicioString1.c
+++ exercicioString1.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Escreva um programa que substitui as ocorrencias de um caractere ‘0’ em uma string por ˆ
 //outro caractere ‘1’.
 
 
-char* frase; //usei ponteiro para treinar
+
 
 
 int main()
 {
-    int i;
-    frase = (char*) malloc(sizeof(char));
+    int i, len;
+    char *frase = (char*) malloc(sizeof(char)+1);
+
 
     printf("Insira a frase: ");
-    scanf("%s", frase);
+    gets(frase);
+    len = strlen(frase);
 
    
-   for (i=0; i<sizeof(frase); i++) {
+   for (i=0; i<len; i++) {
 
-    if (frase[i] = '0')
+    if (frase[i] == '0')
     {
         frase[i] = '1';
     }
@@ -27,6 +30,7 @@ int main()
    }
 
    printf("%s", frase);
+ 
 
-    return 0;
+  
 }
