0a7ed8859e6f3741d38de3cb68da68430f8594ae
Rafael Freitas <rafaelsfreitas283@gmail.com>
2023-03-28 01:11:14 -0300

    modified:   Exercicios/pilhas/exercicio1.c 	modified:   Exercicios/pilhas/output/exercicio1.exe

--- Exercicios/pilhas/exercicio1.c
+++ Exercicios/pilhas/exercicio1.c
@@ -19,14 +19,6 @@ void RESET (PILHA *p){
 }
 
 
-bool Empty(PILHA *pilha) {
-
-
-
-
-
-}
-
 void PUSH (int dado, PILHA *p) {
 
     NO *ptr = (NO*) malloc(sizeof(NO));
@@ -38,12 +30,10 @@ void PUSH (int dado, PILHA *p) {
         ptr->prox = p->topo;
         p->topo = ptr;
     }
-
-
 }
 
 
-void pop(PILHA *p){
+int pop(PILHA *p){
     NO *ptr = p->topo;
     int dado;
 
@@ -54,9 +44,46 @@ void pop(PILHA *p){
         ptr->prox = NULL;
         dado = ptr->dado;
         free(ptr);
+        return dado;
+    }
+}
+
+void imprimePilha(PILHA *p){
+    NO *ptr = p->topo;
+    if(ptr == NULL) {
+        printf ("pilha vazia");
+        return;
+    } else {
+        while (ptr != NULL) {
+            printf("%d", ptr->dado);
+            ptr = ptr->prox;
+        }
+        printf("\n");
+    }
+}
+
+int main () {
+
+    PILHA *p1 = (PILHA* )malloc(sizeof(PILHA));
+    if (p1 == NULL) {
+        printf("erro de alocacao");
+        exit(0);
+    } else {
+        RESET(p1);
+        PUSH (10,p1);
+        PUSH (20, p1);
+        PUSH (30, p1);
+        imprimePilha(p1);
+
+        printf("Desempilha - resultado %d\n", pop(p1));
+        imprimePilha(p1);
+        printf("Desempilha - resultado %d\n", pop(p1));
+        imprimePilha(p1);
+        printf("Desempilha - resultado %d\n", pop(p1));
     }
 
-    
+
+    return 0;
 }
 
 
